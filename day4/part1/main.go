5980b42d7308d5f0a1011ab9e37b0dab23c3b6a4
A4BHI <manthukafc@gmail.com>
2025-12-04 18:21:21 +0530

    Did  some code

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
+
+	input := `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+	slicesOfwalls := strings.Split(input, "\n")
 	var wall [][]rune
+
+	for _, slices := range slicesOfwalls {
+		wall = append(wall, []rune(slices))
+	}
+
+	for _, row := range wall {
+		fmt.Println(string(row))
+	}
 
 }

351c38ca2656e8eae4496966555f262c209b13eb
A4BHI <manthukafc@gmail.com>
2025-12-04 17:12:30 +0530

    ntuhffjsjdnfjsfjn

--- day3/second-part/main.go
+++ day3/second-part/main.go
@@ -11,41 +11,37 @@ func main() {
 811111111111119
 234234234234278
 818181911112111`
+
 	input = strings.TrimSpace(input)
-	allBanks := strings.Split(input, "\n")
+	banks := strings.Split(input, "\n")
 
 	var result []string
-	for _, bank := range allBanks {
+	max := 12
+
+	for _, bank := range banks {
+		bank = strings.TrimSpace(bank)
+		toRemove := len(bank) - max
 		stack := []rune{}
-		max := 12
 
-		for i, ch := range bank {
-			bank = strings.TrimSpace(bank)
-			remaining := len(bank) - i - 1
-
-			for len(stack) > 0 && ch > stack[len(stack)-1] && len(stack)-1+remaining >= max {
+		for _, ch := range bank {
+			fmt.Println(len(stack))
+			for len(stack) > 0 && toRemove > 0 && ch > stack[len(stack)-1] {
 				stack = stack[:len(stack)-1]
+				toRemove--
 			}
-
 			stack = append(stack, ch)
 		}
 
-		if len(stack) > max {
-			stack = stack[:max]
-		}
-
+		stack = stack[:max]
 		result = append(result, string(stack))
 	}
+
 	var sum int
 	for _, v := range result {
-		bank, _ := strconv.Atoi(v)
-		sum += bank
+		num, _ := strconv.Atoi(v)
+		sum += num
 	}
+
 	fmt.Println(result)
 	fmt.Println(sum)
-
-	// stack = append(stack, 1, 2, 3, 5)
-	// stack = stack[:len(stack)-1]
-	// fmt.Println(stack)
-
 }

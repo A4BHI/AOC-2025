106af355df72072f4d8044f3901a0671fb04aabe
A4BHI <manthukafc@gmail.com>
2025-12-05 23:12:08 +0530

    90% Completed

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -33,7 +33,7 @@ func main() {
 	var limit int
 	var counter int = 0
 	for i := 0; i < len(wall); i++ {
-		// fmt.Println(string(wall[i]))
+
 		for j := 0; j < len(wall[i]); j++ {
 			if wall[i][j] == '@' {
 
@@ -51,54 +51,62 @@ func main() {
 				}
 
 				for m := begin; m < limit-1; m++ {
-					// fmt.Println(begin)
-					// fmt.Println(limit)
-					// if j == k {
-					// 	continue
-					// }
-					// fmt.Println("Found0")
-					// fmt.Println(string(wall[i][limit]))
+
 					if wall[i][begin] == '@' || wall[i][begin] == 'X' {
+
 						counter++
-						// wall[i][begin] = 'B'
-						// fmt.Println("Found2")
 
 					}
 
 					if wall[i][limit] == '@' || wall[i][limit] == 'X' {
 						counter++
-						// fmt.Println()
-						// wall[i][limit] = 'E'
-						// fmt.Println("Found")
+
+					}
+
+					if i != 0 {
+						if wall[i-1][k] == '@' || wall[i-1][k] == 'X' {
+							counter++
+						}
+
+						if wall[i-1][begin] == '@' || wall[i-1][begin] == 'X' {
+							counter++
+						}
+						if wall[i-1][limit] == '@' || wall[i-1][limit] == 'X' {
+							counter++
+						}
+
+					}
+
+					if i != len(wall)-1 {
+						if wall[i+1][k] == '@' || wall[i+1][k] == 'X' {
+							counter++
+						}
+
+						if wall[i+1][begin] == '@' || wall[i+1][begin] == 'X' {
+							counter++
+						}
+						if wall[i+1][limit] == '@' || wall[i+1][limit] == 'X' {
+							counter++
+						}
+
 					}
 
 				}
-				// fmt.Println(counter)
 
-				if counter == 2 {
+				if counter < 4 {
 					wall[i][k] = 'X'
-					// fmt.Println("Couter value 2")
-				}
 
-				if counter == 1 {
-					// fmt.Println("Couter value 1")
 				}
 
 				counter = 0
-				// fmt.Println(counter)
-				// if wall[i][j+1]
 
-			} else {
-				fmt.Println("NOP")
 			}
-
 		}
-		fmt.Println()
+
 	}
 
 	for _, row := range wall {
 		fmt.Println(string(row))
 	}
-	// fmt.Println(wall)
 
 }

7872fd3f1f72ef07b988a8260ff5da47ee0622e3
A4BHI <manthukafc@gmail.com>
2025-12-06 00:31:23 +0530

    Entire code structure doesnt contain any logic....but i will fix it,i wont stop

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -32,26 +32,37 @@ func main() {
 	var begin int
 	var limit int
 	var counter int = 0
+	var k int
 	for i := 0; i < len(wall); i++ {
 
 		for j := 0; j < len(wall[i]); j++ {
 			if wall[i][j] == '@' {
 
-				k := j
+				k = j
 				if j == 0 {
 					begin = len(wall[i][:j])
 				} else {
-					begin = len(wall[i][:j-1])
+					if j == 0 {
+						begin = len(wall[i][:j])
+					} else {
+						begin = len(wall[i][:j-1])
+					}
+
 				}
 
 				if j == len(wall[i])-1 {
 					limit = len(wall[i][:j])
 				} else {
-					limit = len(wall[i][:j+1])
+
+					if j == 0 {
+						limit = len(wall[i][:j])
+					} else {
+						limit = len(wall[i][:j+1])
+					}
 				}
 
 				for m := begin; m < limit-1; m++ {
-
+					fmt.Println("Iteration", i, "Begin: ", begin, " Limit: ", limit-1)
 					if wall[i][begin] == '@' || wall[i][begin] == 'X' {
 
 						counter++
@@ -93,20 +104,31 @@ func main() {
 
 				}
 
-				if counter < 4 {
-					wall[i][k] = 'X'
-
-				}
-
-				counter = 0
-
 			}
 		}
+		fmt.Println("Counter: ", counter)
+
+		if counter < 4 {
+			wall[i][k] = 'X'
+
+		}
+
+		counter = 0
 
 	}
 
 	for _, row := range wall {
 		fmt.Println(string(row))
 	}
+	var sum int
+	for i := 0; i < len(wall); i++ {
+		for j := 0; j < len(wall[i]); j++ {
+			if wall[i][j] == 'X' {
+				sum++
+			}
+		}
+
+	}
+	fmt.Println(sum)
 
 }
